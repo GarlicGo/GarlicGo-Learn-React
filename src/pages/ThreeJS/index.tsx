a7b110430f2572d0d0ef2682e7e1c6aae57700d8
GarlicGo <582149912@qq.com>
2022-11-10 21:01:04 +0800

    feat: add fiber test component

--- src/pages/ThreeJS/index.tsx
+++ src/pages/ThreeJS/index.tsx
@@ -1,20 +1,40 @@
 /* eslint-disable react/no-unknown-property */
 import React from 'react';
 import RotateBox from './components/RotateBox';
+import TestFiber from './components/TestFiber';
 import './index.less';
 
 const prefix = 'page-three-js';
 
+interface ShowItemProps {
+  title: string;
+  description?: string;
+  element: React.ReactNode;
+}
+
+const ShowItem: React.FC<ShowItemProps> = ({
+  title,
+  description,
+  element,
+}) => {
+  return (
+    <>
+      <div className={`${prefix}-item-title`}>
+        <h2>{title}</h2>
+        <div>{description}</div>
+      </div>
+      <div className={`${prefix}-item-container`}>
+        {element}
+      </div>
+    </>
+  );
+};
+
 const ThreeJS = () => {
   return (
     <div className={prefix}>
-      <div className={`${prefix}-item-title`}>
-        <h2>Rotate Box</h2>
-        <div>@react-three/fiber Readme Demo</div>
-      </div>
-      <div className={`${prefix}-item-container`}>
-        <RotateBox />
-      </div>
+      <ShowItem title="Fiber Test" element={<TestFiber />} />
+      <ShowItem title="Rotate Box" description="@react-three/fiber Readme Demo" element={<RotateBox />} />
     </div>
   );
 };
