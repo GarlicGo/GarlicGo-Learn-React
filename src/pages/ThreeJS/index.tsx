584a856b15012dadaae88c4c6cc08201e43f03d9
GarlicGo <582149912@qq.com>
2022-11-15 14:37:07 +0800

    feat: 添加了提示

--- src/pages/ThreeJS/index.tsx
+++ src/pages/ThreeJS/index.tsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/no-unknown-property */
-import classNames from 'classnames';
-import React from 'react';
-import RotateBox from './components/RotateBox';
-import TwoDimPlane from './components/TwoDimPlane';
-import ThreeDimPlane from './components/ThreeDimPlane';
-import ThreeDimMove from './components/ThreeDimMove';
-import ThreeDimPlaneLock from './components/ThreeDimPlaneLock';
-import './index.less';
+import classNames from "classnames";
+import React from "react";
+import { Tag, Space } from "@douyinfe/semi-ui";
+import RotateBox from "./components/RotateBox";
+import TwoDimPlane from "./components/TwoDimPlane";
+import ThreeDimPlane from "./components/ThreeDimPlane";
+import ThreeDimMove from "./components/ThreeDimMove";
+import ThreeDimPlaneLock from "./components/ThreeDimPlaneLock";
+import "./index.less";
 
-const prefix = 'page-three-js';
+const prefix = "page-three-js";
 
 interface ShowItemProps {
   title: string;
   description?: string;
+  newLineDescription?: {
+    tag: string;
+    text: string;
+  };
   element: React.ReactNode;
   hasBorder?: boolean;
 }
@@ -20,18 +25,31 @@ interface ShowItemProps {
 const ShowItem: React.FC<ShowItemProps> = ({
   title,
   description,
+  newLineDescription,
   element,
   hasBorder = true,
 }) => {
   return (
     <>
       <div className={`${prefix}-item-title`}>
-        <h2>{title}</h2>
+        <h2 className={`${prefix}-item-title-h`}>{title}</h2>
         <div>{description}</div>
       </div>
-      <div className={classNames({
-        [`${prefix}-item-container`]: hasBorder,
-      })}>
+      {newLineDescription && (
+        <div className={`${prefix}-item-new-line-des`}>
+          <Space>
+            {newLineDescription.tag && newLineDescription.tag !== "" && (
+              <Tag color="blue" type="ghost">{newLineDescription.tag}</Tag>
+            )}
+            {newLineDescription.text}
+          </Space>
+        </div>
+      )}
+      <div
+        className={classNames({
+          [`${prefix}-item-container`]: hasBorder,
+        })}
+      >
         {element}
       </div>
     </>
@@ -51,19 +69,34 @@ const ThreeJS = () => {
       />
       <ShowItem
         title="3D Camera 维度锁定下的 2D 平面"
-        description="快捷键提示：双指(右键)可以拖动画布"
+        newLineDescription={{
+          tag: "快捷键提示",
+          text: "双指缩放(或鼠标滚轮)拖动画布、双指(或鼠标右键)拖动拖动画布",
+        }}
         element={<ThreeDimPlaneLock />}
       />
       <ShowItem
         title="3D Camera 下的 2D 平面"
+        newLineDescription={{
+          tag: "快捷键提示",
+          text: "单指(或鼠标左键)拖动旋转镜头、双指缩放(或鼠标滚轮)拖动画布、双指(或鼠标右键)拖动拖动画布",
+        }}
         element={<TwoDimPlane />}
       />
       <ShowItem
         title="3D Camera 下的 3D 物体"
+        newLineDescription={{
+          tag: "快捷键提示",
+          text: "单指(或鼠标左键)拖动旋转镜头、双指缩放(或鼠标滚轮)拖动画布、双指(或鼠标右键)拖动拖动画布",
+        }}
         element={<ThreeDimPlane />}
       />
       <ShowItem
         title="物体运动"
+        newLineDescription={{
+          tag: "快捷键提示",
+          text: "单指(或鼠标左键)拖动旋转镜头、双指缩放(或鼠标滚轮)拖动画布、双指(或鼠标右键)拖动拖动画布",
+        }}
         element={<ThreeDimMove />}
         hasBorder={false}
       />
